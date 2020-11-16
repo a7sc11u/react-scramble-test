9c90ef1d57ebe4b8c4a1a3a4f98fefc4156d0acb
Apostolos Christodoulou <a7sc11u@gmail.com>
2020-11-16 17:07:11 +0000

    ui edits

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import React, { useCallback, useState } from "react"
+import React, { useCallback, useEffect, useState } from "react"
 import { useTweaks } from "use-tweaks"
 import { css } from "@emotion/css"
-import { loremIpsum } from "lorem-ipsum"
+import { LoremIpsum } from "lorem-ipsum"
+
+const lorem = new LoremIpsum({
+  sentencesPerParagraph: {
+    max: 8,
+    min: 4,
+  },
+  wordsPerSentence: {
+    max: 16,
+    min: 4,
+  },
+})
 
 import { TextScramble } from "./text-scramble"
 
 import "./styles.css"
 
 export default function App() {
-  const [sample, setSample] = useState(loremIpsum())
-
-  const config = useTweaks("Config", {
-    play: true,
-    fontSize: { value: 20, min: 1, max: 32, step: 1 },
-    leading: { value: 1.5, min: 0, max: 3, step: 0.01 },
-    tracking: { value: 0.6, min: -0.2, max: 3, step: 0.01 },
-  })
+  const [sample, setSample] = useState(lorem.generateSentences(1))
 
   const tweaks = useTweaks("Scramble", {
+    play: true,
     speed: { value: 0.4, min: 0.1, max: 1, step: 0.1 },
     scramble: { value: 5, min: 0, max: 42, step: 1 },
     seed: { value: 6, min: 0, max: 42, step: 1 },
@@ -26,27 +31,40 @@ export default function App() {
     stepInterval: { value: 1, min: 1, max: 20, step: 1 },
   })
 
+  const text = useTweaks("Text", {
+    sentences: { value: 1, min: 1, max: 10, step: 1 },
+    fontSize: { value: 20, min: 1, max: 32, step: 1 },
+    leading: { value: 1.5, min: 0, max: 3, step: 0.01 },
+    tracking: { value: 0.6, min: -0.2, max: 3, step: 0.01 },
+    measure: { value: 50, min: 25, max: 75, step: 1 },
+  })
+
   const handleComplete = useCallback(() => {
     setTimeout(() => {
-      setSample(loremIpsum())
+      const sample = lorem.generateSentences(text.sentences)
+      setSample(sample)
     }, 850)
-  }, [])
+  }, [text.sentences])
+
+  useEffect(() => {
+    const sample = lorem.generateSentences(text.sentences)
+    setSample(sample)
+  }, [text.sentences])
 
   return (
     <div className="App">
       <div>
         <TextScramble
-          play={config.play}
           text={sample}
           {...tweaks}
           onComplete={handleComplete}
           className={css`
             display: block;
             font-family: "Courier New", Courier, monospace;
-            letter-spacing: ${config.tracking}em;
-            line-height: ${config.leading};
-            max-width: 50ch;
-            font-size: ${config.fontSize}px;
+            letter-spacing: ${text.tracking}em;
+            line-height: ${text.leading};
+            max-width: ${text.measure}ch;
+            font-size: ${text.fontSize}px;
           `}
         />
       </div>
